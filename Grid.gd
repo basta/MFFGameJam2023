d816a1ec04dbec8ee612ed321e67b6bc1880a691
Keiky <you@example.com>
2023-05-05 22:19:20 +0200

    Grid using Color

--- Grid.gd
+++ Grid.gd
@@ -7,8 +7,8 @@ extends Node2D
 @export var SPRITE_SIZE = 32
 
 
-var data_matrix := []
-var sprite_matrix := []
+var data_matrix : Array = [] 		# matrix of Colors
+var sprite_matrix : Array = []		# matrix of Sprite references
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,21 +20,21 @@ func _ready():
 			var sprite = $"NodeTemplate".duplicate()
 			sprite.position = Vector2(col * SPRITE_SIZE, row * SPRITE_SIZE)
 			add_child(sprite)
-			data_matrix[row].append(0)
+			data_matrix[row].append(Color.WHITE)
 			sprite_matrix[row].append(sprite)
 	$"NodeTemplate".visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(data_matrix)
+	pass
 
 
 func apply_stamp(pos_x, pos_y, stamp_matrix):
 	for row in range(stamp_matrix.size()):
 		for col in range(stamp_matrix[0].size()):
 			data_matrix[pos_x+row][pos_y+col] = stamp_matrix[row][col]
-		
+			sprite_matrix[pos_x+row][pos_y+col].self_modulate(stamp_matrix[row][col])
 
 
 func do():
--- Stamp.gd
+++ Stamp.gd
@@ -4,12 +4,12 @@ extends Node2D
 @export var SIZE_X := 3
 @export var SIZE_Y := 3
 
-var matrix := []
+var matrix : Array[] = []		# array of Colors
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
