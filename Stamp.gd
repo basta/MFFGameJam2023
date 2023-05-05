b778d8dbe37764923c237e54ab878b18bc3132d6
Keiky <you@example.com>
2023-05-05 22:09:52 +0200

    Stamp

---
+++ Stamp.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+@export var SIZE_X := 3
+@export var SIZE_Y := 3
+
+var matrix := []
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
