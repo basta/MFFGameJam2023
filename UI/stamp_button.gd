bf6f66fc4daf4bab27e36b846480be7f4945d03f
basta <jsembasta@gmail.com>
2023-05-05 22:10:06 +0200

    UI Stuff

---
+++ UI/stamp_button.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_up():
+	Input.set_custom_mouse_cursor(texture_normal) # Replace with function body.
