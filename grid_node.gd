1338a30cd56a61a06d8b3885fa45bd18333bbb80
Keiky <you@example.com>
2023-05-06 12:05:28 +0200

    cursor wip

--- grid_node.gd
+++ grid_node.gd
@@ -3,13 +3,6 @@ extends Node2D
 class_name GridNode
 
 
-@export var SPRITE_SIZE = 32 + 2
-
-
-func set_grid_position(row, col):
-	position = Vector2(col * SPRITE_SIZE, row * SPRITE_SIZE)
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
