5cb19f8f6330d4f3b9efd5b2a0fa0c722f082095
basta <jsembasta@gmail.com>
2023-05-06 11:19:11 +0200

    scene

---
+++ UI/stamp_button_ctr.gd
@@ -0,0 +1,14 @@
+extends MarginContainer
+
+class_name stamp_button_ctr
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func add_stamp_ui(node : Node):
+	add_child(node)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
